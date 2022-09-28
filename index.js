bfd4ef6fd39150199e792d46d7d28186c24ea83a
chshen1998 <chewhoashen@gmail.com>
2022-09-28 13:10:13 +0800

    Express app

---
+++ index.js
@@ -0,0 +1,24 @@
+import express, { Router } from "express";
+import cors from "cors";
+
+import {getModules, createModule, deleteModule, updateModule} from './controllers/module-controller.js'
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // config cors so that front-end can use
+app.options("*", cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World from OTOT-B");
+});
+
+app.get("/module", (req, res) => getModules(req, res));
+
+app.post("/module", (req, res) => createModule(req, res))
+
+app.delete("/module/:moduleCode", (req, res) => deleteModule(req, res))
+
+app.put("/module", (req, res) => updateModule(req ,res))
+
+app.listen(8000);
---
+++ model/module-orm.js
@@ -0,0 +1,45 @@
+import { createModule, getModules, findModule, deleteModule, updateModule } from "./repository.js";
+
+export async function ormCreateModule(moduleCode, moduleTitle) {
+    try {
+        const newModule = await createModule(moduleCode, moduleTitle)
+        newModule.save()
+        return newModule        
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export async function ormGetModules() {
+    try {
+        return await getModules()
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
+
+export async function ormFindModule(moduleCode) {
+    try {
+        const module = await findModule(moduleCode)
+        return module        
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export async function ormDeleteModule(moduleCode) {
+    try {
+        await deleteModule(moduleCode)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export async function ormUpdateModule(moduleCode, moduleTitle) {
+    try {
+        await updateModule(moduleCode, moduleTitle)
+    } catch (err) {
+        console.log(err)
+    }
+}
